5918987a88d13c953b2ba57329170a559c8c11df
Zuniga Carlos Gadiel <138547044+u22222779@users.noreply.github.com>
2026-06-24 14:28:17 -0500

    eliminación de noticia php

--- conx.php
+++
@@ -1,5 +0,0 @@
-<?php
-$link = mysqli_connect("localhost", "sedirorg_userisi", "Hhv7x63Kt9D") or die ('Error es: '.mysqli_error());
-mysqli_select_db($link, "sedirorg_db");
-$tildes = $link->query("SET NAMES 'utf8'"); //Para que se inserten las tildes correctamente
-
